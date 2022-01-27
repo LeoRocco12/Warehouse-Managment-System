1c97cc7d9dfff5f9554fd877d0348ecb057c0a1f
LeoRocco12 <leo.rocca03@gmail.com>
2022-01-27 08:58:47 +0100

    Update Index.php
    Scheletro Completo

--- Index.php
+++ Index.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <title>Home Page</title>
+    <meta charset="UTF-8" />
+    <style>
+    </style>
   </head>
   <body>
+      <h1>Home Page Progetto!!</h1>
+      <?php
+
+
+      ?>
   </body>
 </html>
